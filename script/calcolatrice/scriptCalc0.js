c85c6092c0c32bd1507a96fbfc3c1ac283c934ee
Riccardo Marzi <78096957+CursedRicky@users.noreply.github.com>
2024-10-25 10:09:51 +0200

    Update scriptCalc0.js
    Script calcolatrice in js con due input e 4 pulsanti operatori + pulsante uguale

--- script/calcolatrice/scriptCalc0.js
+++ script/calcolatrice/scriptCalc0.js
@@ -1,1 +1,41 @@
+let value1;
+let value2;
 
+let operator;
+
+function getValues() {
+    value1 = parseFloat(document.getElementById("value1").value)
+    value2 = parseFloat(document.getElementById("value2").value)
+}
+
+function add() {
+    operator = "+"
+}
+
+function sott() {
+    operator = "-"
+}
+
+function molt() {
+    operator = "*"
+}
+
+function divi() {
+    operator = "/"
+}
+
+function calcolo() {
+    getValues()
+    if (operator == "+") {
+        document.getElementById("result").innerHTML = String(value1 + " + " + value2 + " = " + parseFloat(value1+value2))
+    } else if (operator == "-") {
+        document.getElementById("result").innerHTML = String(value1 + " - " + value2 + " = " + parseFloat(value1-value2))
+    } else if (operator == "*") {
+        document.getElementById("result").innerHTML = String(value1 + " * " + value2 + " = " + parseFloat(value1*value2))
+    } else if (operator == "/") {
+        document.getElementById("result").innerHTML = String(value1 + " / " + value2 + " = " + parseFloat(value1/value2))
+    } else {
+        alert("Operatore non valido")
+    }
+      
+}
